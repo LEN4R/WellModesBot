3eea051e589356a07d29787133e27d059c6e7512
LEN4R <len4r@mail.ru>
2022-04-21 22:38:05 +0500

    v1.0.1

--- src/Program.cs
+++ src/Program.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace WellModesBot
@@ -36,31 +38,47 @@ namespace WellModesBot
             if (msg.Text != null)
             {
                 var message = new StringBuilder();
-
+                //await client.SendTextMessageAsync(msg.Chat.Id, FullName, replyMarkup: GetButtons());
                 switch (msg.Text)
                 {
                     case "info":
                         msg = await client.SendTextMessageAsync(
-                              chatId: msg.Chat.Id, text: $"Разработка: Галиев Ленар Разимович \nВерсия: 1.0.0 (Beta)",
+                              chatId: msg.Chat.Id, text: $"Разработка: Галиев Ленар Разимович \n" +
+                                                         $"Версия: 1.0.1\n" +
+                                                         $"С помощью данного бота вы можете полудить режимыне данные к свкажинам."
+,
                               replyToMessageId: msg.MessageId); 
                         msg = await client.SendStickerAsync(chatId: msg.Chat.Id, 
                               sticker: "https://tlgrm.ru/_/stickers/18f/4d5/18f4d57e-c910-3aef-9523-9a0d3bb60468/9.webp");
-                    break;
+                        break;
+                    case "versia":
+                        msg = await client.SendTextMessageAsync(
+                              chatId: msg.Chat.Id, text: $"[Версия: 1.0.0 (Beta)] \n * Добавлена возможность вывода скважин с разными месторождениями \n" +
+                                                         $"[Версия: 1.0.1] \n * При выводе данных добавлены единицы изменерия \n * Убран баг вывода при вводе info \n * Убран баг некорректного вывода скважин (555)",
+                              replyToMessageId: msg.MessageId);
+                        msg = await client.SendStickerAsync(chatId: msg.Chat.Id,
+                              sticker: "https://cdn.tlgrm.app/stickers/18f/4d5/18f4d57e-c910-3aef-9523-9a0d3bb60468/192/3.webp");
+                        break;
+                    case "batman":
+                        msg = await client.SendStickerAsync(chatId: msg.Chat.Id,
+                              sticker: "https://tlgrm.ru/_/stickers/fa2/27e/fa227e13-45d9-3821-9b19-018dab84e820/192/5.webp");
+                        break;
 
                     default:
+                        var requiredData = new[]  5, 7, 8, 3, 17, 11, 14, 15, 18, 22, 23, 33, 34, 38, 39, 51, 54, 55, 56 };
                         if (_fields.TryGetValue(msg.Text, out var list))
                         {
-                            message.AppendLine();
-
+                            message.AppendLine("Возможные варианты:");
                             for (int i = 0; i < list.Count; i++)
-                                message.AppendLine($"{i + 1}. {list[i]}");
+                                message.AppendLine($"{i+1}. {list[i]}");
 
                             if (list.Count == 1)
                             {
-                                PrintFieldDataByColumnIndexes(list[0], message, new[] { 5, 7, 8, 17, 11, 14, 15, 18, 22, 23, 33, 38, 39, 44, 51, 54, 55, 56 });
+                                PrintFieldDataByColumnIndexes(list[0], message, requiredData);
                             }
 
                             await SendMessage(msg, message.ToString());
+                            //await client.SendTextMessageAsync(msg.Chat.Id, message.ToString(), replyMarkup: GetButtons());
                         }
                         else
                         {
@@ -68,7 +86,7 @@ namespace WellModesBot
 
                             if (firstField != null)
                             {
-                                PrintFieldDataByColumnIndexes(firstField, message, new[] { 5, 7, 8, 17, 11, 14, 15, 18, 22, 23, 33, 38, 39, 44, 51, 54, 55, 56 });
+                                PrintFieldDataByColumnIndexes(firstField, message, requiredData);
                                 await SendMessage(msg, message.ToString());
                             }
                             else
@@ -80,6 +98,19 @@ namespace WellModesBot
                 }
             }
         }
+
+        /*
+        private static IReplyMarkup GetButtons()
+        {
+            return new ReplyKeyboardRemove
+            {
+                Selective = new List<List<KeyboardButton>>
+                {
+
+                }
+            };
+        }
+        */
 
         private static async Task SendMessage(Telegram.Bot.Types.Message msg, string message)
         {
@@ -103,7 +134,6 @@ namespace WellModesBot
                 message.AppendLine($"{query[index].key}: {query[index].value} {query[index].metrics}");
             }
         }
-
         public static void GetData()
         {
             var path = @"ТРДС.xlsx";
@@ -112,6 +142,11 @@ namespace WellModesBot
             using (var xlPackage = new ExcelPackage(new FileInfo(path)))
             {
                 var myWorksheet = xlPackage.Workbook.Worksheets.First(); //select sheet here
+
+                foreach (var item in xlPackage.Workbook.Worksheets)
+                {
+
+                }
 
                 var totalRows = myWorksheet.Dimension.End.Row;
                 var totalColumns = myWorksheet.Dimension.End.Column;
